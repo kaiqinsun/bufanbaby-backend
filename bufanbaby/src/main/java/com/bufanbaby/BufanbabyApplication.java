dd884f166b559dcd426dd5e829fbf949f86931a3
kaiqinsun <kaiqinsun@gmail.com>
2015-02-15 17:05:48 -0500

    Dev environment setup using gradle

---
+++ bufanbaby/src/main/java/com/bufanbaby/BufanbabyApplication.java
@@ -0,0 +1,12 @@
+package com.bufanbaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BufanbabyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BufanbabyApplication.class, args);
+    }
+}
---
+++ bufanbaby/src/test/java/com/bufanbaby/BufanbabyApplicationTests.java
@@ -0,0 +1,18 @@
+package com.bufanbaby;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BufanbabyApplication.class)
+@WebAppConfiguration
+public class BufanbabyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
